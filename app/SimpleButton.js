c87f1557399942a5bdf5d684e30e20c1e3ad28d7
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-11-26 10:46:02 +0000

    Add NoteScreen view

--- app/SimpleButton.js
+++ app/SimpleButton.js
@@ -2,15 +2,23 @@ import React, {Component} from "react";
 
 import {
   Text,
-  View
+  View,
+  TouchableOpacity,
 } from 'react-native';
 
 export default class SimpleButton extends Component {
   render () {
     return (
-      <View>
-        <Text>Simple Button</Text>
-      </View>
+      <TouchableOpacity onPress={this.props.onPress}>
+        <View>
+          <Text>{this.props.customText || 'Simple Button'}</Text>
+        </View>
+      </TouchableOpacity>
     );
   }
+}
+
+SimpleButton.propTypes = {
+  onPress: React.PropTypes.func.isRequired,
+  customText: React.PropTypes.string
 }
