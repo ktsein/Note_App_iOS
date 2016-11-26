58eb818ddd3d1cb80ad2f07dfebabe33434b5550
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-11-26 12:05:00 +0000

    Navigation bar working, debugging success

---
+++ app/HomeScreen.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+import {
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+
+export default class HomeScreen extends Component {
+  render(){
+    return (
+      <View style= {styles.container}>
+        <Text>Home</Text>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
