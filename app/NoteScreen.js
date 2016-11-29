0a01eee0b71f108efef6dc3d1743c87f1066dbf5
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-11-29 12:41:59 +0000

    Add note screen styling

--- app/NoteScreen.js
+++ app/NoteScreen.js
@@ -11,17 +11,22 @@ export default class NoteScreen extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <TextInput
-          ref="title"
-          autoFocus={true}
-          placeholder="Untitled"
-          style={styles.title}
-          onEndEditing={(text) => {this.refs.body.focus()}}
+        <View style={styles.inputContainer}>
+          <TextInput
+            ref="title"
+            autoFocus={true}
+            autoCapitalize="sentences"
+            placeholder="Untitled"
+            style={[styles.title , styles.textInput]}
+            onEndEditing={(text) => {this.refs.body.focus()}}
           />
-        <TextInput
-          ref="body"
-          multiline={true}
-          placeholder="Start typing" style={styles.body} />
+        </View>
+          <TextInput
+            ref="body"
+            multiline={true}
+            placeholder="Start typing"
+            style={[styles.body , styles.textInput]}
+          />
       </View>
     );
   }
@@ -39,5 +44,16 @@ var styles = StyleSheet.create({
   },
   body: {
     flex: 1
+  },
+  inputContainer: {
+    borderBottomColor: '#9E7CE3',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    marginBottom: 10
+  },
+  textInput: {
+    flex: 1,
+    fontSize: 16,
   }
+
 });
